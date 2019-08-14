368bb882dfb9d63e8b656497c583aca402f0abaf
Michael Ash <mash@umass.edu>
2019-08-14 15:26:06 -0700

    Revised row operations in octave script

--- row-operations.m
+++ row-operations.m
@@ -1,46 +1,59 @@
 ## Octave program
 
-## This is the solution to the set of equality constraints that gives
-## the cost-minimization problem in Meadows or Malls
-
-## [ GR GD AR AD MR MD ]
-
-A = [1,1,0,0,0,0,300;  0,0,1,1,0,0,100;  0,0,0,0,1,1,150;  0,1,0,1,0,1,300;  0,1,1,0,0,0,100;  0,0,0,0,1,0,0]
-
-b = [300 100 150 300 100 0 ]'
-A_1 = inv(A(:,1:6))
-A_1 * b 
+## This is the solution to the particular set of equality constraints
+## that gives the cost-minimization problem in Meadows or Malls
 
 
-# Swap rows 5 and 6
-A([5 6], :) = A([6 5], :)
 
-# Replace row 3 with row 3 less row 5
-A(3,:) = A(3,:) - A(5,:)
+## x = [ GR AR MR GD AD MD ]
 
-# Swap rows 3 and 6
-A([3 6], :) = A([6 3], :)
+c = [50 200 100 500 2000 1000]
 
-# Replace row 4 with row 4 less row 6
-A(4,:) = A(4,:) - A(6,:)
 
-# Swap rows 2 and 4
-A([2 4], :) = A([4 2], :)
+A = [1,0,0,1,0,0;  0,1,0,0,1,0;  0,0,1,0,0,1;  0,0,0,1,1,1;  0,0,1,0,0,0;  0,1,0,1,0,0]
+b = [300 100 150 300 0 100 ]'
 
-A(3,:) = A(3,:) - A(2,:)
+## Solve by inverting A
+A_1 = inv(A)
+A_1 * b
 
-A(4,:) = A(4,:) + A(3,:)
+## Solve by row operations on the augmented matrix 
+A_aug = [A b]
 
-A(3,:) = A(3,:) - 1/2 * A(4,:)
+## Replace row 6 with row 6 less row 2
+A_aug(6, :) = A_aug(6, :) - A_aug(2, :)
 
-A([3 4], :) = A([4 3], :)
+## Switch rows 3 and 5
+A_aug([3 5], :) = A_aug([5 3], :)
 
-A(2,:) = A(2,:)  + A(4,:)
+## Replace row 5 with row 5 less row 3
+A_aug(5, :) = A_aug(5, :) - A_aug(3, :)
 
-A(1,:) = A(1,:) - A(2,:)
+## Replace row 6 with row 6 less row 4
+A_aug(6, :) = A_aug(6, :) - A_aug(4, :)
 
-A(3,:) = A(3,:)/2
+## Switch rows 5 and 6
+A_aug([5 6], :) = A_aug([6 5], :)
 
-A(4,:) = -1 * A(4,:)
+## Divide row 5 by -2
+A_aug(5, :) = A_aug(5, :) / -2
 
+## The matrix is now upper triangular and I could solve by substitution.
+## Or continue with row operations
+
+## Replace row 5 with row 5 less one half of row 6 
+A_aug(5, :) = A_aug(5, :) - A_aug(6, :) / (2)
+
+## Replace row 4 with row 4 less row 6 and then subtract row 5
+A_aug(4, :) = A_aug(4, :) - A_aug(6,:) - A_aug(5,:)
+
+## Replace row 2 with row 2 less row 5
+A_aug(2, :) = A_aug(2, :) - A_aug(5,:)
+
+## Replace row 1 with row 1 less row 4
+A_aug(1, :) = A_aug(1, :) - A_aug(4,:)
+
+
+## Compute the cost of this solution
+dot(c,  A_aug(:, 7))
 
